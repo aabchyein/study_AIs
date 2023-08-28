8ba68e820db78c7a4a99959d73b0d31fac513d92
aabchyein <abcqaz369@naver.com>
2023-08-28 18:09:00 +0900

    23-08-28 혜인

--- codes/MLs/Regressions/RecurrenceOfSurgery_regression_quest.py
+++ codes/MLs/Regressions/RecurrenceOfSurgery_regression_quest.py
@@ -1,6 +1,6 @@
 # 사용자에게 입력받은 설명변수 값 3개를 변수에 담기
 # 체중, 수숡시간의 D.type이 float 형식
-# input() return str() -> float()
+# input() return str() -> int() or float()
 height = int(input('신장'))
 weight = float(input('체중'))
 ot = float(input('수술시간'))
